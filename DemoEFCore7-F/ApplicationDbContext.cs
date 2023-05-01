5b0b730b050dd9ac2dff11dd948bbddd79a5ef24
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-05-01 11:30:10 -0500

    added class ApplicationDbContext and Person

---
+++ DemoEFCore7-F/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEFCore7_F
+{
+    public class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=MVELASQUEZ;Database=DemoEFCore;Integrated Security=True");
+        }
+    }
+}
